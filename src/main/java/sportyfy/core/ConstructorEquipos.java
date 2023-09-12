ea1fc2f99ba65598d5ae0c3bd6920874d122ce9b
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-12 12:29:11 -0300

    Clase ConstructorEquipos, sin método implementado

---
+++ src/main/java/sportyfy/core/ConstructorEquipos.java
@@ -0,0 +1,17 @@
+package sportyfy.core;
+
+import lombok.Data;
+
+import sportyfy.core.futbol.Equipo;
+
+import java.util.List;
+
+@Data
+public class ConstructorEquipos {
+    private List<Equipo> equipos;
+
+    //TODO: Implementar método crearEquipos que lea los JSONS y cree los equipos con sus partidos
+    public void crearEquipos() {
+
+    }
+}
