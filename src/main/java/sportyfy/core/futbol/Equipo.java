c11fb44e947e5ab7a35d884b07b1bfeea1e17fed
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-12 14:38:20 -0300

    Agrego el id para que funcione bien el crearEquipos. El toString es de prueba.

--- src/main/java/sportyfy/core/futbol/Equipo.java
+++ src/main/java/sportyfy/core/futbol/Equipo.java
@@ -2,12 +2,15 @@ package sportyfy.core.futbol;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
 @EqualsAndHashCode
+@ToString
 public class Equipo {
+    private Integer id;
     private String nombre;
     private List<Partido> partidos;
 }
