b790adb8aa4f4df417612727a37f311984696d66
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-12 14:37:04 -0300

    Implemento una versión del crearEquipo sin partidos.

--- src/main/java/sportyfy/core/ConstructorEquipos.java
+++ src/main/java/sportyfy/core/ConstructorEquipos.java
@@ -1,17 +1,31 @@
 package sportyfy.core;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 
 import sportyfy.core.futbol.Equipo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Data
 public class ConstructorEquipos {
     private List<Equipo> equipos;
 
-    //TODO: Implementar método crearEquipos que lea los JSONS y cree los equipos con sus partidos
+    // TODO: Implementar método crearEquipos que lea los JSONS y cree los equipos
+    // con sus partidos
     public void crearEquipos() {
+        ObjectMapper mapper = new ObjectMapper();
+        String ruta = "datosFutbol/equipos/equipos.json";
 
+        try {
+            // Lee el archivo JSON y mapea su contenido a una lista de objetos Equipo
+            File jsonFile = new File(ruta);
+            equipos = mapper.readValue(jsonFile, new TypeReference<List<Equipo>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
