6b629a18fa5cedbade4b89d2e0dfa3a7dde1e2f7
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-12 12:28:55 -0300

    Clase BuscadorPronosticadores sin método implementado

---
+++ src/main/java/sportyfy/core/BuscadorPronosticadores.java
@@ -0,0 +1,15 @@
+package sportyfy.core;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class BuscadorPronosticadores {
+    private Set<Pronosticador> pronosticadores;
+
+    //TODO: Implementar método encontrarPronosticadores que reciba una ruta y devuelva un Set de Pronosticadores
+    public Set<Pronosticador> encontrarPronosticadores(String ruta) {
+        return null;
+    }
+}
