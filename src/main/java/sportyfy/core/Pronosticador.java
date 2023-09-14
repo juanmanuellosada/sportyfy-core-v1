cfca24d196dc98117ece4fc0f39c104eef9a159e
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-13 22:03:04 -0300

    Agrego que el pronosticador reciba los partidos

--- src/main/java/sportyfy/core/Pronosticador.java
+++ src/main/java/sportyfy/core/Pronosticador.java
@@ -1,8 +1,12 @@
 package sportyfy.core;
 
+import java.util.List;
+
 import sportyfy.core.futbol.Equipo;
+import sportyfy.core.futbol.Partido;
 
 public interface Pronosticador {
-    public Pronostico pronosticar(Equipo equipoLocal, Equipo equipoVisitante);
+    public Pronostico pronosticar(Equipo equipoLocal, Equipo equipoVisitante, List<Partido> partidosAnteriores);
+
     public String obtenerDeporte();
 }
