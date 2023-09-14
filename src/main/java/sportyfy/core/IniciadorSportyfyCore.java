3827c8d9b5f7b4d3c4c30861987f1bd31d75bf92
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-13 23:14:55 -0300

    Test de discovery

--- src/main/java/sportyfy/core/IniciadorSportyfyCore.java
+++ src/main/java/sportyfy/core/IniciadorSportyfyCore.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import sportyfy.core.futbol.Equipo;
 import sportyfy.core.futbol.Partido;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @Data
@@ -13,7 +14,7 @@ public class IniciadorSportyfyCore {
     private List<Equipo> equipos;
     private List<Partido> partidos;
 
-    public void iniciar(String rutaArchivoEquipos, String rutaCarpetaPartidos, String rutaPronosticadores) {
+    public void iniciar(String rutaArchivoEquipos, String rutaCarpetaPartidos, String rutaPronosticadores) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
         ConstructorEquipos constructorEquipos = new ConstructorEquipos();
         this.equipos = constructorEquipos.crearEquipos(rutaArchivoEquipos);
 
@@ -21,7 +22,18 @@ public class IniciadorSportyfyCore {
         this.partidos = constructorPartidos.crearPartidos(rutaCarpetaPartidos, equipos);
 
         this.buscadorPronosticadores = new BuscadorPronosticadores();
-        this.buscadorPronosticadores.buscarPronosticadores(rutaPronosticadores);
+        buscadorPronosticadores.setPronosticadores(buscadorPronosticadores.buscarPronosticadores(rutaPronosticadores));
+
+        for (Pronosticador pronosticador : buscadorPronosticadores.getPronosticadores()) {
+            Pronostico pronostico = pronosticador.pronosticar(equipos.get(0), equipos.get(1), partidos);
+
+            System.out.println("Pronóstico para " + pronosticador.getClass().getSimpleName() + ": " + pronostico.getEquipoGanador());
+        }
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        IniciadorSportyfyCore iniciadorSportyfyCore = new IniciadorSportyfyCore();
+        iniciadorSportyfyCore.iniciar("datosFutbol/equipos/equipos.json", "datosFutbol/ultimos_resultados/", "src/pronosticadores");
     }
 
     // Dejo este método porque lo usa el test de ejemplo, pero no iría
