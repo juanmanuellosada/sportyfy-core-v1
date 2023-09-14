85aa736fff428e6c2179b83942988459c4a5997a
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-09-13 21:40:57 -0300

    Agrego los equipos y partidos como variables y su construcción

--- src/main/java/sportyfy/core/IniciadorSportyfyCore.java
+++ src/main/java/sportyfy/core/IniciadorSportyfyCore.java
@@ -1,22 +1,30 @@
 package sportyfy.core;
 
 import lombok.Data;
+import sportyfy.core.futbol.Equipo;
+import sportyfy.core.futbol.Partido;
+
+import java.util.List;
 
 @Data
 public class IniciadorSportyfyCore {
 
-    private ConstructorEquipos constructorEquipos;
     private BuscadorPronosticadores buscadorPronosticadores;
+    private List<Equipo> equipos;
+    private List<Partido> partidos;
 
-    public void iniciar(String ruta) {
-        this.constructorEquipos = new ConstructorEquipos();
-        this.constructorEquipos.crearEquipos();
+    public void iniciar(String rutaArchivoEquipos, String rutaCarpetaPartidos, String rutaPronosticadores) {
+        ConstructorEquipos constructorEquipos = new ConstructorEquipos();
+        this.equipos = constructorEquipos.crearEquipos(rutaArchivoEquipos);
+
+        ConstructorPartidos constructorPartidos = new ConstructorPartidos();
+        this.partidos = constructorPartidos.crearPartidos(rutaCarpetaPartidos, equipos);
 
         this.buscadorPronosticadores = new BuscadorPronosticadores();
-        this.buscadorPronosticadores.encontrarPronosticadores(ruta);
+        this.buscadorPronosticadores.buscarPronosticadores(rutaPronosticadores);
     }
 
-    //Dejo este método porque lo usa el test de ejemplo, pero no iría
+    // Dejo este método porque lo usa el test de ejemplo, pero no iría
     public String saludar() {
         return "¡Hola mundo!";
     }
