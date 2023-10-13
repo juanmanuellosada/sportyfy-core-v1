878578b96be7dee5ce4fe77ecec2083b92aaee1b
Maria Sol <mariasol.hoyos@hotmail.com>
2023-10-12 21:56:20 -0300

    agrego PronosticoNullParaHistorial

---
+++ src/main/java/sportyfy/core/PronosticoNullParaHistorial.java
@@ -0,0 +1,7 @@
+package sportyfy.core;
+
+public class PronosticoNullParaHistorial extends PronosticoParaHistorial {
+    public PronosticoNullParaHistorial(){
+        super(null,null,null);
+    }
+}
