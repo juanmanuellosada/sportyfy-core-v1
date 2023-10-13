554070986ee0538dcba26a1ac9fe2fec459d1170
Maria Sol <mariasol.hoyos@hotmail.com>
2023-10-12 22:56:41 -0300

    cambio PronosticoNullParaHistorial

--- src/main/java/sportyfy/core/PronosticoNullParaHistorial.java
+++ src/main/java/sportyfy/core/PronosticoNullParaHistorial.java
@@ -2,11 +2,8 @@ package sportyfy.core;
 import sportyfy.core.entidades.equipo.Equipo;
 
 public class PronosticoNullParaHistorial extends PronosticoParaHistorial {
-    private Equipo equipoLocal;
-    private Equipo equipoVisitante;
-    private Equipo equipoGanador;
 
-    public PronosticoNullParaHistorial(Equipo equipoLocal, Equipo equipoVisitante, Equipo equipoGanador) {
-        super(equipoLocal, equipoVisitante, null);
+    public PronosticoNullParaHistorial() {
+        super(null, null, null);
     }
 }
