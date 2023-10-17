f52391267f9971ca5c651e1ca02e5e7b35346c41
Juanma Losada <juanmalosada01@gmail.com>
2023-10-17 15:47:13 -0300

    Quito print del pronóstico

--- src/main/java/sportyfy/core/core/SportyfyCore.java
+++ src/main/java/sportyfy/core/core/SportyfyCore.java
@@ -68,8 +68,6 @@ public class SportyfyCore extends Observable {
 
         setPronosticoActual(pronosticador.pronosticar(partidoFuturo, partidosJugados));
 
-        System.out.println(pronosticoActual);
-
         setChanged();
         notifyObservers();
     }
